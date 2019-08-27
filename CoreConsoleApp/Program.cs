397f1deaf26d3855f46fe18f701d0af0eae8013f
SimutinAS <andrey.s32@yandex.ru>
2019-08-27 19:27:25 +0300

    Function

--- CoreConsoleApp/Program.cs
+++ CoreConsoleApp/Program.cs
@@ -1,12 +1,20 @@
 ﻿using StandardClassLibrary;
 using System;
+using System.Linq.Expressions;
 
 namespace CoreConsoleApp
 {
     class Program
     {
+        static T Function<T>(Func<T, T, T> fnc, T left, T right)
+        {
+            return fnc.Invoke(left, right);
+        }
+
         static void Main(string[] args)
         {
+            var res = Function((x, y) => x + y, 2, 3);
+
             Console.WriteLine($"EnvVar: {AppConfig.EnvVar}");
             Console.WriteLine(AppConfig.GetConnectionString("Connection1"));
             Console.WriteLine(AppConfig.GetValue<string>("Family"));
