b5327fb9662b7306c4a719c10d8d6cf0ae12a6e6
SimutinAS <s95simutin@gmail.com>
2019-09-09 21:58:48 +0300

    currencylib

---
+++ CurrencyLib/CurrencyMongo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CurrencyLib
+{
+    public class CurrencyMongo
+    {
+        [BsonId]
+        public ObjectId Id;
+    }
+}
---
+++ CurrencyLib/MongoDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MongoWrapper;
+
+namespace CurrencyLib
+{
+    public static class MongoDao
+    {
+        private static readonly MongoWrap Mongo = MongoWrap.FromConfig("MongoConnection");
+
+    }
+}
