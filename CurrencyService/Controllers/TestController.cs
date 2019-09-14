af183263c79d9cc844edbc55ce6de46e11b0c1ec
SimutinAS <s95simutin@gmail.com>
2019-09-14 22:52:40 +0300

    test controller

---
+++ CurrencyService/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("method")]
+        public ActionResult<string> Method(bool str)
+        {
+            if (str == true)
+                return "asd";
+            return Unauthorized();
+        }
+    }
+}
