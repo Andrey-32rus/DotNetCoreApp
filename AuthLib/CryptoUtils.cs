4db6d4c130bf675e64901b59a4a590fe8ab10ede
SimutinAS <s95simutin@gmail.com>
2019-09-22 17:44:08 +0300

    sha512

---
+++ AuthLib/CryptoUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthLib
+{
+    public static class CryptoUtils
+    {
+        private static readonly SHA512 Sha512 = SHA512.Create();
+
+        public static string GetSha512(string source)
+        {
+            var bytes = Sha512.ComputeHash(Encoding.UTF8.GetBytes(source));
+
+            StringBuilder hex = new StringBuilder(bytes.Length * 2);
+            foreach (byte b in bytes)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+    }
+}
---
+++ NUnitTestProject/Crypto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AuthLib;
+using NUnit.Framework;
+
+namespace NUnitTestProject
+{
+    public class Crypto
+    {
+        [Test]
+        public void TestSha512()
+        {
+            string src = "asd12345";
+            string hash = CryptoUtils.GetSha512(src);
+            Console.WriteLine(hash);
+        }
+    }
+}
