f1569a4371187ab944b1bdb1f9017123cec6f797
SimutinAS <s95simutin@gmail.com>
2019-09-25 19:28:25 +0300

    размеры

--- NUnitTestProject/Crypto.cs
+++ NUnitTestProject/Crypto.cs
@@ -15,5 +15,23 @@ namespace NUnitTestProject
             string hash = CryptoUtils.GetSha512Base64Encoded(src);
             Console.WriteLine(hash);
         }
+
+        [Test]
+        public void FromHexToBase64()
+        {
+            string src = "asd12345";
+            string sha512HexString = "ef2c7b78cc9cacfb1826" +
+                                     "2d2ed698cdcca598d413946" +
+                                     "ed0d743285d889c87ad29ecbf3f9" +
+                                     "de43338dd1c06d33dfbb43a4bc95aa" +
+                                     "c2301e63a426bdd3e2a582f2da7";
+            int hexLength = sha512HexString.Length;
+
+            string sha512Base64 = CryptoUtils.GetSha512Base64Encoded(src);
+            int base64Length = sha512Base64.Length;
+
+            string toBase64 = CryptoUtils.HexStringToBase64(sha512HexString);
+            Assert.AreEqual(sha512Base64, toBase64);
+        }
     }
 }
--- UtilsLib/CryptoUtils.cs
+++ UtilsLib/CryptoUtils.cs
@@ -9,6 +9,12 @@ namespace UtilsLib
     {
         private static readonly SHA512 Sha512 = SHA512.Create();
 
+        public static string HexStringToBase64(string source)
+        {
+            var bytes = Encoding.UTF8.GetBytes(source);
+            return Convert.ToBase64String(bytes);
+        }
+
         public static string GetSha512Base64Encoded(string source)
         {
             var bytes = Sha512.ComputeHash(Encoding.UTF8.GetBytes(source));
