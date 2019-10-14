d083b0142395e0b28d35ef19f31ddd95dc9bf28b
SimutinAS <s95simutin@gmail.com>
2019-10-14 20:39:08 +0300

    linq tests

---
+++ NUnitTestProject/LinqTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using NUnit.Framework;
+
+namespace NUnitTestProject
+{
+    public class LinqTests
+    {
+        [Test]
+        public void Order()
+        {
+            List<int> list = new List<int> {525, 323, 99, 123, 52};
+            list.Sort((left, right) =>
+            {
+                if (left > right)
+                    return 1;
+                if (left < right)
+                    return -1;
+
+                return 0;
+            });
+            var ordered = list.OrderByDescending(x => x);
+        }
+    }
+}
