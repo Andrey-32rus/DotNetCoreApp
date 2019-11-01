404b45a420e7671e81b38e9c520abc23268258ba
SimutinAS <s95simutin@gmail.com>
2019-11-01 20:41:10 +0300

    mongo

---
+++ NUnitTestProject/Models/RatingAggMongo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NUnitTestProject.Models
+{
+    [BsonIgnoreExtraElements]
+    public class RatingAggMongo
+    {
+        [BsonId]
+        public ObjectId Id;
+        public int user;
+        public ulong Rating;
+    }
+}
---
+++ NUnitTestProject/MongoDbLearn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoWrapper;
+using NUnit.Framework;
+using NUnitTestProject.Models;
+
+namespace NUnitTestProject
+{
+    public class MongoDbLearn
+    {
+        [Test]
+        public void ForkDocs()
+        {
+            var wrap = new MongoWrap("mongodb://localhost:27017");
+            var col = wrap.GetCollection<RatingAggMongo>("Test", "RatingAgg");
+            var all = col.Find(FilterDefinition<RatingAggMongo>.Empty).Limit(2048).ToList();
+            foreach (var rt in all)
+            {
+                rt.Id = ObjectId.Empty;
+                rt.user = 5;
+            }
+
+            col.InsertMany(all);
+        }
+    }
+}
