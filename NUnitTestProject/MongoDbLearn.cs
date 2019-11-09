189850899206b4c958e4142705dc410330de5cb0
SimutinAS <s95simutin@gmail.com>
2019-11-10 02:16:32 +0300

    побаловался с транзакциями

--- NUnitTestProject/MongoDbLearn.cs
+++ NUnitTestProject/MongoDbLearn.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
@@ -28,32 +29,68 @@ namespace NUnitTestProject
             col.InsertMany(all);
         }
 
-        [Test]
-        public void Trx()
+        public void Trx(MongoClient client)
         {
-            var client = new MongoClient("mongodb://localhost:27017");
-            var col = client.GetDatabase("Test").GetCollection<TransactionsModel>( "Transactions");
+            var col = client.GetDatabase("Test").GetCollection<TransactionsModel>("Transactions");
 
             var filter = Builders<TransactionsModel>.Filter.Eq(x => x.Id, ObjectId.Parse("5dc6ecb496709870ec8ffbdf"));
             var lockUpdate = Builders<TransactionsModel>.Update.Set(x => x.TrxLock, ObjectId.GenerateNewId());
             var update = Builders<TransactionsModel>.Update.Inc(x => x.Counter, 1);
-
+            
             using (var session = client.StartSession())
             {
-                session.StartTransaction();
-                try
+                session.WithTransaction((ses, token) =>
                 {
-                    col.UpdateOne(session, filter, lockUpdate);
-                    Thread.Sleep(6);
-                    col.UpdateOne(session, filter, update);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    session.AbortTransaction();
-                }
-                session.CommitTransaction();
+                    try
+                    {
+                        col.UpdateOne(ses, filter, lockUpdate);
+                        col.UpdateOne(ses, filter, update);
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                });
+                //session.StartTransaction();
+                //try
+                //{
+                //    col.UpdateOne(session, filter, lockUpdate);
+                //    //Thread.Sleep(5);
+                //    col.UpdateOne(session, filter, update);
+                //}
+                //catch (Exception e)
+                //{
+                //    Console.WriteLine(e);
+                //    session.AbortTransaction();
+                //    throw;
+                //}
+                //session.CommitTransaction();
             }
+        }
+
+        [Test]
+        public void TrxTest()
+        {
+            var connSettings = new MongoClientSettings
+            {
+                ConnectionMode = ConnectionMode.ReplicaSet,
+                ReadPreference = ReadPreference.Primary,
+                ReplicaSetName = "rs0",
+                Servers = new MongoServerAddress[]
+                {
+                    new MongoServerAddress("localhost", 27017), 
+                }
+            };
+            
+            //var client = new MongoClient("mongodb://localhost:27017");
+            var client = new MongoClient(connSettings);
+            Task.Run(() => Trx(client));
+            Thread.Sleep(1000);
+            Task.Run(() => Trx(client));
+
+            Thread.Sleep(5000);
         }
     }
 }
