9e844f6e34a1a12a77e5369ea7f0335811abd122
SimutinAS <s95simutin@gmail.com>
2019-11-09 21:01:48 +0300

    mongo

---
+++ NUnitTestProject/Models/TransactionsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NUnitTestProject.Models
+{
+    [BsonIgnoreExtraElements]
+    public class TransactionsModel
+    {
+        [BsonId]
+        public ObjectId Id;
+        public int Counter;
+        public ObjectId TrxLock;
+    }
+}
--- NUnitTestProject/MongoDbLearn.cs
+++ NUnitTestProject/MongoDbLearn.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using MongoWrapper;
 using NUnit.Framework;
@@ -24,6 +26,34 @@ namespace NUnitTestProject
             }
 
             col.InsertMany(all);
+        }
+
+        [Test]
+        public void Trx()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var col = client.GetDatabase("Test").GetCollection<TransactionsModel>( "Transactions");
+
+            var filter = Builders<TransactionsModel>.Filter.Eq(x => x.Id, ObjectId.Parse("5dc6ecb496709870ec8ffbdf"));
+            var lockUpdate = Builders<TransactionsModel>.Update.Set(x => x.TrxLock, ObjectId.GenerateNewId());
+            var update = Builders<TransactionsModel>.Update.Inc(x => x.Counter, 1);
+
+            using (var session = client.StartSession())
+            {
+                session.StartTransaction();
+                try
+                {
+                    col.UpdateOne(session, filter, lockUpdate);
+                    Thread.Sleep(6);
+                    col.UpdateOne(session, filter, update);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    session.AbortTransaction();
+                }
+                session.CommitTransaction();
+            }
         }
     }
 }
