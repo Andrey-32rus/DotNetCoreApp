06cc5af27c0f4a1548414b9bfb53c790eaaa1bbf
SimutinAS <s95simutin@gmail.com>
2019-11-30 19:07:35 +0300

    async dispose

---
+++ NUnitTestProject/DisposeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using NUnitTestProject.Models;
+
+namespace NUnitTestProject
+{
+    public class DisposeTest
+    {
+        [Test]
+        public void TestAsyncDispose()
+        {
+            DisposableClass obj = null;
+            try
+            {
+                obj = new DisposableClass();
+            }
+            finally
+            {
+                obj?.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ NUnitTestProject/Models/DisposableClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestProject.Models
+{
+    public class DisposableClass : IAsyncDisposable
+    {
+        public async ValueTask DisposeAsync()
+        {
+            await new ValueTask(Task.CompletedTask);
+        }
+    }
+}
