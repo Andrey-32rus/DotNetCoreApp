a5b5f878fdd3b6b439c99af1f9dfc80c28da0349
SimutinAS <s95simutin@gmail.com>
2019-12-19 21:46:40 +0300

    dt tests

---
+++ NUnitTestProject/DateTimes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnitTestProject
+{
+    public class DateTimes
+    {
+        [Test]
+        public void Datetime()
+        {
+            var now = DateTimeOffset.Now;
+            var utcNow = DateTimeOffset.UtcNow;
+
+            var dt = DateTime.UtcNow;
+        }
+    }
+}
