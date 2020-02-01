9f603ad1becad1cb3a615319841ea8fc078f26d2
SimutinAS <s95simutin@gmail.com>
2020-02-02 00:18:49 +0300

    closure

---
+++ NUnitTestProject/Closure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace NUnitTestProject
+{
+    public class Closure
+    {
+        public int Ariphmetic(int left, int right, Func<int, int, int> fnc)
+        {
+            return fnc.Invoke(left, right);
+        }
+
+        public void ClosureDisAsm()
+        {
+            int x = 3;
+            int result = Ariphmetic(5, 2, (left, right) => left + right + x);
+        }
+    }
+}
