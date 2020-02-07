8c660980b424db0e7cbaed882c9697c1b482f2c9
SimutinAS <s95simutin@gmail.com>
2020-02-07 22:05:25 +0300

    контейенер хелсчека, но он тут нах не нужен

---
+++ WeatherService/Controllers/HealthcheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WeatherService/HealthCheckContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeatherService
+{
+    public sealed class HealthCheckContainer
+    {
+        public bool IsValid { get; private set; }
+
+        public HealthCheckContainer()
+        {
+            IsValid = false;
+        }
+
+        public void SetValid()
+        {
+            if(IsValid == true)
+                throw new Exception("сервис уже валиден, неверное действие");
+
+            IsValid = true;
+        }
+    }
+}
