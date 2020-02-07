8699945223b6e81128b31415499dc9c82c8e6bb8
SimutinAS <s95simutin@gmail.com>
2020-02-07 21:48:20 +0300

    env controller

---
+++ WeatherService/Controllers/EnvironmentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EnvironmentController : ControllerBase
+    {
+        private readonly IWebHostEnvironment env;
+
+        public EnvironmentController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return env.EnvironmentName;
+        }
+    }
+}
