06bb209204139f67a65f64ce2c11f9d722f94f64
SimutinAS <s95simutin@gmail.com>
2020-02-17 13:55:50 +0300

    rws

---
+++ ConsoleApp/RWLock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class RWLock : IDisposable
+    {
+        public struct WriteLockToken : IDisposable
+        {
+            private readonly ReaderWriterLockSlim rw;
+            public WriteLockToken(ReaderWriterLockSlim rw)
+            {
+                this.rw = rw;
+                rw.EnterWriteLock();
+            }
+            public void Dispose()
+            {
+                rw.ExitWriteLock();
+            }
+        }
+
+        public struct ReadLockToken : IDisposable
+        {
+            private readonly ReaderWriterLockSlim rw;
+            public ReadLockToken(ReaderWriterLockSlim rw)
+            {
+                this.rw = rw;
+                rw.EnterReadLock();
+            }
+            public void Dispose()
+            {
+                rw.ExitReadLock();
+            }
+        }
+
+        private readonly ReaderWriterLockSlim rw = new ReaderWriterLockSlim();
+
+        public ReadLockToken ReadLock()
+        {
+            return new ReadLockToken(rw);
+        }
+
+        public WriteLockToken WriteLock()
+        {
+            return new WriteLockToken(rw);
+        }
+
+        public void Dispose()
+        {
+            rw.Dispose();
+        }
+    }
+}
---
+++ ConsoleApp/RWStrategy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class RwStrategy : IDisposable, IAsyncDisposable
+    {
+        private readonly ReaderWriterLockSlim rw = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+
+        public void SurroundReadLock(Action action)
+        {
+            rw.EnterReadLock();
+            try
+            {
+                action.Invoke();
+            }
+            finally
+            {
+                rw.ExitReadLock();
+            }
+        }
+
+        public void SurroundWriteLock(Action action)
+        {
+            rw.EnterWriteLock();
+            try
+            {
+                action.Invoke();
+            }
+            finally
+            {
+                rw.ExitWriteLock();
+            }
+        }
+
+        public void Dispose()
+        {
+            rw?.Dispose();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return new ValueTask(Task.Run(Dispose));
+        }
+    }
+}
