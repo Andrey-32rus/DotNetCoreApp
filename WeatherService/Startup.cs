fc4af4915e8c36a5347dd4abd2736e00220a4293
SimutinAS <s95simutin@gmail.com>
2020-02-17 20:21:13 +0300

    newtonsoft

--- WeatherService/Startup.cs
+++ WeatherService/Startup.cs
@@ -40,7 +40,7 @@ namespace WeatherService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSingleton(new ALog("Singleton"));
             services.AddSingleton(svcColl =>
             {
