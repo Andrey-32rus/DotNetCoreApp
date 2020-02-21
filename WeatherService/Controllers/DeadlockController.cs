f02e328f081fc88a5a8af6fd641da6197ef2e3bc
SimutinAS <s95simutin@gmail.com>
2020-02-21 23:02:16 +0300

    дедлок которого нет

---
+++ WeatherService/Controllers/DeadlockController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeadlockController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var service = new SomeService();
+            service.DoSomeJobAsync().Wait();
+
+            return Ok();
+        }
+    }
+
+    public class SomeService
+    {
+        public async Task DoSomeJobAsync()
+        {
+            await Task.Delay(200);
+            Thread.Sleep(100);
+        }
+    }
+}
