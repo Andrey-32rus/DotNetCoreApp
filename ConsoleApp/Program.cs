60ddd1283d8bc7e626abb82389dc6b4971f57642
SimutinAS <s95simutin@gmail.com>
2020-02-23 19:39:20 +0300

    default builder

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,27 +14,15 @@ namespace ConsoleApp
             services.AddSingleton<ALog>(new ALog("ConsoleApp"));
         }
 
-        static void ConfigureAppConfiguration(HostBuilderContext host, IConfigurationBuilder configuration)
-        {
-            configuration
-                .AddEnvironmentVariables()
-                .AddJsonFile("appsettings.json", true, false)
-                .AddJsonFile($"appsettings.{host.HostingEnvironment.EnvironmentName}.json", true, false);
-        }
-
-
         static void Main(string[] args)
         {
-            var host = new HostBuilder()
+            var host = Host.CreateDefaultBuilder(args)
                 .UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
-                .ConfigureAppConfiguration(ConfigureAppConfiguration)
                 .ConfigureServices(ConfigureServices)
                 .Build();
 
             var program = host.Services.GetRequiredService<ProgramService>();
             program.Main(args);
-
-            //host.Run();
         }
     }
 }
