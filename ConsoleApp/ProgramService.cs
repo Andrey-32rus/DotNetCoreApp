f97774e17213aeb8938628bf047da777f65b3ebd
SimutinAS <s95simutin@gmail.com>
2020-02-23 18:37:57 +0300

    env console

--- ConsoleApp/ProgramService.cs
+++ ConsoleApp/ProgramService.cs
@@ -12,33 +12,25 @@ namespace ConsoleApp
     public class ProgramService
     {
         private IConfiguration configuration;
-        private IHostEnvironment host;
+        private IHostEnvironment env;
         private ALog logger;
 
-        public ProgramService(IConfiguration cfg, IHostEnvironment host, ALog logger)
+        public ProgramService(IConfiguration cfg, IHostEnvironment env, ALog logger)
         {
             configuration = cfg;
-            this.host = host;
+            this.env = env;
             this.logger = logger;
         }
 
         public void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!");
-            logger.Info($"Environment: {host.EnvironmentName}", "main");
+            logger.Info($"Environment: {env.EnvironmentName}", "main");
 
-            Transaction tr = new Transaction();
-            tr.PerformTransaction();
-
-            for (int i = 0; i < 2; i++)
-            {
-                Task.Run(() =>
-                {
-                    Console.WriteLine(new DateTimeOffset(tr.LastTransaction).ToUnixTimeSeconds());
-                });
-            }
+            Console.WriteLine(env.ContentRootPath);
 
             Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.ReadLine();
         }
     }
 }
