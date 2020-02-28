65be9cbce9efb56fa363d42a015d9e0c9928aa21
SimutinAS <s95simutin@gmail.com>
2020-02-29 00:33:40 +0300

    ref value speed

---
+++ XUnitTestProject/Models/RefValueModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestProject.Models
+{
+    public class RefModel
+    {
+        public int Int1;
+        public long Long1;
+        public double Double1;
+        public decimal Decimal1;
+
+        public int Int2;
+        public long Long2;
+        public double Double2;
+        public decimal Decimal2;
+    }
+
+    public struct ValueModel
+    {
+        public int Int1;
+        public long Long1;
+        public double Double1;
+        public decimal Decimal1;
+
+        public int Int2;
+        public long Long2;
+        public double Double2;
+        public decimal Decimal2;
+    }
+}
---
+++ XUnitTestProject/RefValueSpeed.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xunit;
+using XUnitTestProject.Models;
+
+namespace XUnitTestProject
+{
+    public class RefValueSpeed
+    {
+        private int length = 1000000;
+        private List<RefModel> refList;
+        private List<ValueModel> valueList;
+
+        public RefValueSpeed()
+        {
+            refList = ListFill<RefModel>(length);
+            valueList = ListFill<ValueModel>(length);
+        }
+
+        private static List<T> ListFill<T>(int listLength) where T : new()
+        {
+            List<T> list = new List<T>(listLength);
+            for (int i = 0; i < listLength; i++)
+            {
+                T model = new T();
+                list.Add(model);
+            }
+
+            return list;
+        }
+
+        private static void ListForeach<T>(List<T> list)
+        {
+            foreach (var refModel in list)
+            {
+                var tmpModel1 = refModel;
+                var tmpModel2 = refModel;
+                var tmpModel3 = refModel;
+                var tmpModel4 = refModel;
+                var tmpModel5 = refModel;
+            }
+        }
+
+        [Fact]
+        public void RefListFill()
+        {
+            var list = ListFill<RefModel>(length);
+        }
+
+        [Fact]
+        public void ValueListFill()
+        {
+            var list = ListFill<ValueModel>(length);
+        }
+
+        [Fact]
+        public void RefListForeach()
+        {
+            ListForeach(refList);
+        }
+
+        [Fact]
+        public void ValueListForeach()
+        {
+            ListForeach(valueList);
+        }
+    }
+}
