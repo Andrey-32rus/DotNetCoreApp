f5d4100fb37bf535225e8f06115fd225ce2c9df6
SimutinAS <s95simutin@gmail.com>
2020-02-29 22:56:02 +0300

    when all

---
+++ XUnitTestProject/TaskTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class TaskTests
+    {
+        [Fact]
+        public async Task WhenAll()
+        {
+            int maxSec = 5;
+            List<Task> tasks = new List<Task>(maxSec);
+            for (int i = 0; i < maxSec; i++)
+            {
+                var task = Task.Delay(TimeSpan.FromSeconds(i + 1));
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
