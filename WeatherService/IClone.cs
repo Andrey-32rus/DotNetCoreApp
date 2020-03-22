d1f16350078202c14b3852d1b34408647ef6c108
SimutinAS <s95simutin@gmail.com>
2020-03-22 15:50:18 +0300

    iclone

---
+++ WeatherService/IClone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherService
+{
+    public interface IClone<T>
+    {
+        IClone<T> GetClone();
+    }
+
+    public class Cloneable : IClone<Cloneable>
+    {
+        private int field;
+
+        public Cloneable(int field)
+        {
+            this.field = field;
+        }
+
+        public Cloneable(Cloneable clone)
+        {
+            this.field = clone.field;
+        }
+
+        public IClone<Cloneable> GetClone()
+        {
+            return new Cloneable(this);
+        }
+    }
+}
