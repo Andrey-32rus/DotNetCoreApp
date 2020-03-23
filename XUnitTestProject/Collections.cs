39573f591a63d2d436b56482afbfcc4677f26cd4
SimutinAS <s95simutin@gmail.com>
2020-03-23 17:35:06 +0300

    colls

---
+++ XUnitTestProject/Collections.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class Collections
+    {
+        private void ListIteration<T>(IReadOnlyCollection<T> list)
+        {
+            foreach (var elem in list)
+            {
+                
+            }
+        }
+
+        [Fact]
+        public void ArrayImplementsIList()
+        {
+            var arr = new string[]
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            var list = new List<string>(arr);
+
+            ListIteration(arr);
+        }
+    }
+}
