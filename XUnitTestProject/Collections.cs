d10106afb37e2a210cf491190721a95eeb7f4362
SimutinAS <andrey.s32.work@yandex.ru>
2020-05-28 20:08:04 +0300

    immutable array

--- XUnitTestProject/Collections.cs
+++ XUnitTestProject/Collections.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace XUnitTestProject
 {
     public class Collections
     {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public Collections(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Immutable()
+        {
+            var immutableArray = new int[] {1, 2, 3, 4, 5}.ToImmutableArray();
+            testOutputHelper.WriteLine(immutableArray.ToString());
+        }
+
         private void ListIteration<T>(IReadOnlyCollection<T> list)
         {
             foreach (var elem in list)
