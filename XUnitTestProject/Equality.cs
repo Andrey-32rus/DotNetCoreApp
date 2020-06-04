8728bc53e64de87e49f6d6e3ee120f9362c485cf
SimutinAS <andrey.s32.work@yandex.ru>
2020-06-04 22:22:51 +0300

    equality

---
+++ XUnitTestProject/Equality.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using XUnitTestProject.Models;
+
+namespace XUnitTestProject
+{
+    public class Equality
+    {
+        [Fact]
+        public void Main()
+        {
+            var person1 = new Person { Id = 1, Name = "Eric" };
+            var person2 = new Person { Id = 1, Name = "Eric" };
+
+            var personList = new List<Person> { person1 };
+            var personDictionary = new Dictionary<Person, int> { { person1, 1 } };
+
+            bool equals = person1.Equals(person2);
+            bool contains = personList.Contains(person2);
+            bool containsKey = personDictionary.ContainsKey(person2);
+            bool hashEquality = person1.GetHashCode() == person2.GetHashCode();
+        }
+    }
+}
---
+++ XUnitTestProject/Models/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestProject.Models
+{
+    public class Person : IEquatable<Person>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+        public bool Equals(Person person)
+        {
+            if (person == null)
+                return false;
+
+            var thisTuple = (Id, Name);
+            var personTuple = (person.Id, person.Name);
+            return thisTuple.Equals(personTuple); // using Tuples
+
+            //person.Id == Id && person.Name == Name;    // or using traditional style
+        }
+
+        public override bool Equals(object obj)
+        {
+            var person = obj as Person;
+            if (person == null)
+                return false;
+
+            return Equals(person);
+        }
+
+        public override int GetHashCode()
+        {
+            var thisTuple = (Id, Name);
+            return thisTuple.GetHashCode();
+        }
+
+
+        //public static bool operator ==(Person p1, Person p2) =>
+        //    (!object.ReferenceEquals(p1, null)) && p1.Equals(p2);
+
+        //public static bool operator !=(Person p1, Person p2) =>
+        //    !(p1 == p2);
+    }
+}
