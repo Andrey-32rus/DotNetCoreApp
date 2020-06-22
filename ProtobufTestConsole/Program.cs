568d4532c13d01d90431ddea5ca6abfa4091aa8e
SimutinAS <andrey.s32.work@yandex.ru>
2020-06-22 17:52:33 +0300

    grpc

---
+++ ProtobufTestConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProtobufTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
