c93a80f1af6114fbd3e5928be525f6c2ae058dcd
SimutinAS <andrey.s32.work@yandex.ru>
2020-06-23 23:05:35 +0300

    protobuf

--- ProtobufTestConsole/Program.cs
+++ ProtobufTestConsole/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using ProtoBuf;
+using ProtobufTestConsole.Protobuf;
 
 namespace ProtobufTestConsole
 {
     class Program
     {
+        private static byte[] SerializeProtobuf<T>(T src)
+        {
+            using MemoryStream protobuf = new MemoryStream();
+            Serializer.Serialize(protobuf, src);
+            return protobuf.ToArray();
+        }
+
+        private static T DeserializeProtobuf<T>(byte[] bytes)
+        {
+            using MemoryStream protobuf = new MemoryStream(bytes);
+            return Serializer.Deserialize<T>(protobuf);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            Model1 model = new Model1
+            {
+                Str = "string",
+                Int = 1,
+            };
+
+            var protobuf = SerializeProtobuf(model);
+            string strProtobuf = Encoding.UTF8.GetString(protobuf);
+            Model1 deserializedModel = DeserializeProtobuf<Model1>(protobuf);
         }
     }
 }
---
+++ ProtobufTestConsole/Protobuf/Model1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+
+namespace ProtobufTestConsole.Protobuf
+{
+    [ProtoContract]
+    public class Model1
+    {
+        [ProtoMember(1)]
+        public string Str { get; set; }
+        [ProtoMember(2)]
+        public int Int { get; set; }
+    }
+}
