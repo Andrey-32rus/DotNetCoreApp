b42c18c995796737e7c80f36560bdff1b3440f99
SimutinAS <andrey.s32.work@yandex.ru>
2020-07-15 21:59:46 +0300

    http client learning

---
+++ HttpClientLearning/MyHttpClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientLearning
+{
+    public class MyHttpClient
+    {
+        private HttpClient client;
+        public MyHttpClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public void Req(int count)
+        {
+            List<Task> tasks = new List<Task>(count);
+            for (int i = 0; i < count; i++)
+            {
+                var task = client.GetAsync("https://vk.com");
+                tasks.Add(task);
+            }
+
+            var resTask = Task.WhenAll(tasks);
+            resTask.Wait();
+        }
+    }
+}
---
+++ HttpClientLearning/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HttpClientLearning
+{
+    class Program
+    {
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient<MyHttpClient>().SetHandlerLifetime(TimeSpan.FromSeconds(1));
+        }
+        static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args).ConfigureServices(ConfigureServices).Build();
+
+            var client = host.Services.GetRequiredService<MyHttpClient>();
+            client.Req(1000);
+        }
+    }
+}
