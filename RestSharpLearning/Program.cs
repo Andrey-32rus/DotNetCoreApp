68ddca49ec453f084308f433532ac1b394b45757
SimutinAS <andrey.s32.work@yandex.ru>
2020-08-15 10:34:27 +0300

    restsharp

---
+++ RestSharpLearning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RestSharp;
+
+namespace RestSharpLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient();
+            var request = new RestRequest("https://www.cbr-xml-daily.ru/daily_json.js");
+            var response = client.Get(request);
+            string content = response.Content;
+            Console.WriteLine(content);
+        }
+    }
+}
