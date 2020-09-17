80a6d60683b591680d7240b18f1e8591dee7548c
SimutinAS <andrey.s32.work@yandex.ru>
2020-09-17 21:14:55 +0300

    signalr learning

---
+++ SignalRLearning/Hubs/SampleHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRLearning.Hubs
+{
+    public class SampleHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
