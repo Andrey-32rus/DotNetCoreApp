6171eaf232c3b2fdf20ddd962d0f5d22547bfc31
SimutinAS <andrey.s32.work@yandex.ru>
2021-03-07 23:40:20 +0300

    nlogextensions

---
+++ LogSessions.NLogExtension/RegisterLogSessions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+using NLog.Web.LayoutRenderers;
+
+namespace LogSessions.NLogExtension
+{
+    public static class RegisterLogSessions
+    {
+        public static void Register()
+        {
+            AspNetLayoutRendererBase.Register("LogKey",
+                (logEventInfo, httpContext, loggingConfiguration)
+                    => httpContext.Items["LogKey"]); // usage ${LogKey}
+
+            AspNetLayoutRendererBase.Register("LogStep",
+                (logEventInfo, httpContext, loggingConfiguration)
+                    =>
+                {
+                    if(httpContext.Items.TryGetValue("LogStep", out object val))
+                    {
+                        int intVal = (int) val;
+                        intVal++;
+                        httpContext.Items["LogStep"] = intVal;
+                        return intVal;
+                    }
+                    
+                    httpContext.Items["LogStep"] = 1;
+                    return 1;
+                }); // usage ${LogStep}
+        }
+    }
+}
