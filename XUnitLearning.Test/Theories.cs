377b35f7da351ba45f26c35858fd11dd3c26849a
SimutinAS <andrey.s32.work@yandex.ru>
2021-05-21 23:29:41 +0300

    theories

---
+++ XUnitLearning.Test/Theories.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitLearning.Test
+{
+    public class Theories
+    {
+        [Theory]
+        [InlineData(2, 4)]
+        [InlineData(3, 9)]
+        public void SquareEquals(int srcValue, int expected)
+        {
+            int actually = srcValue * srcValue;
+            Assert.Equal(expected, actually);
+        }
+    }
+}
