ecab9aad059f636da70336b206a3bf93c3929827
SimutinAS <andrey.s32.work@yandex.ru>
2021-11-09 18:26:50 +0300

    namespace nullable

--- SeleniumLearning/Program.cs
+++ SeleniumLearning/Program.cs
@@ -3,24 +3,23 @@ using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace SeleniumLearning
+namespace SeleniumLearning;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        string driverPath = @"C:\SeleniumDrivers\chromedriver.exe";
+        ChromeOptions options = new ChromeOptions
         {
-            string driverPath = @"C:\SeleniumDrivers\chromedriver.exe";
-            ChromeOptions options = new ChromeOptions
-            {
-            };
+        };
 
-            var dirPath = Path.GetDirectoryName(driverPath);
-            var exePath = Path.GetFileName(driverPath);
+        var dirPath = Path.GetDirectoryName(driverPath);
+        var exePath = Path.GetFileName(driverPath);
 
-            IWebDriver webDriver = new ChromeDriver(ChromeDriverService.CreateDefaultService(dirPath, exePath), options);
-            webDriver.Navigate().GoToUrl("https://yandex.ru");
+        IWebDriver webDriver = new ChromeDriver(ChromeDriverService.CreateDefaultService(dirPath, exePath), options);
+        webDriver.Navigate().GoToUrl("https://yandex.ru");
 
-            Console.ReadLine();
-        }
+        Console.ReadLine();
     }
 }
