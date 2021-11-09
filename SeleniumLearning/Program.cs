f5bc3d7804b66f31ebaa264c7d2b0d661a6cb389
SimutinAS <andrey.s32.work@yandex.ru>
2021-11-09 16:58:58 +0300

    Selenium chrome

---
+++ SeleniumLearning/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumLearning
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string driverPath = @"C:\SeleniumDrivers\chromedriver.exe";
+            ChromeOptions options = new ChromeOptions
+            {
+            };
+
+            var dirPath = Path.GetDirectoryName(driverPath);
+            var exePath = Path.GetFileName(driverPath);
+
+            IWebDriver webDriver = new ChromeDriver(ChromeDriverService.CreateDefaultService(dirPath, exePath), options);
+            webDriver.Navigate().GoToUrl("https://yandex.ru");
+
+            Console.ReadLine();
+        }
+    }
+}
