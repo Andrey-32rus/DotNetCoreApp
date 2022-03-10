8b66a02ae5f0d6df18316ad8f708619564876e88
SimutinAS <andrey.s32.work@yandex.ru>
2022-03-10 20:36:30 +0300

    selenium

--- SeleniumLearning/Program.cs
+++ SeleniumLearning/Program.cs
@@ -12,15 +12,27 @@ namespace SeleniumLearning
             string driverPath = @"C:\SeleniumDrivers\chromedriver.exe";
             ChromeOptions options = new ChromeOptions
             {
+                //BinaryLocation = @"C:\Users\Andrey\AppData\Local\Yandex\YandexBrowser\Application\browser.exe",
+                //BinaryLocation = @"D:\yandexPortable\Browser\browser.exe",
+
+                BinaryLocation = @"D:\GoogleChromePortable\GoogleChromePortable.exe",
+                //BinaryLocation = @"D:\GoogleChromePortable\App\Chrome-bin\chrome.exe",
             };
 
             var dirPath = Path.GetDirectoryName(driverPath);
             var exePath = Path.GetFileName(driverPath);
 
-            IWebDriver webDriver = new ChromeDriver(ChromeDriverService.CreateDefaultService(dirPath, exePath), options);
+            var service = ChromeDriverService.CreateDefaultService(dirPath, exePath);
+            IWebDriver webDriver = new ChromeDriver(service, options);
             
             webDriver.Manage().Window.Maximize();
             webDriver.Navigate().GoToUrl("https://yandex.ru");
+
+            var textStr = webDriver.FindElement(By.Id("text"));
+            textStr.SendKeys("керамогранит");
+
+            var button = webDriver.FindElement(By.CssSelector(".search2__button > button"));
+            button.Click();
 
             Console.ReadLine();
 
