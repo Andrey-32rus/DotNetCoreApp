3d2d059fe643b8dbf13366166d3296104a319198
SimutinAS <andrey.s32.work@yandex.ru>
2022-03-27 22:04:10 +0300

    postgres

---
+++ PostgresqlClientConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using Npgsql;
+
+string connStr = "Host=localhost;Database=database1;Username=postgres;Password=postgres;Persist Security Info=True";
+var conn = new NpgsqlConnection(connStr);
+await using var cmd = new NpgsqlCommand("SELECT * FROM public.tbl", conn);
+await conn.OpenAsync();
+await using var reader = await cmd.ExecuteReaderAsync();
+DataTable dt = new DataTable();
+dt.Load(reader);
+
+Console.WriteLine(dt);
+Console.ReadLine();
