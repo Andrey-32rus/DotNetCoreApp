52f1363805dd3f8d645e8cd64fd4fcb2f9cb7f41
SimutinAS <andrey.s32.work@yandex.ru>
2022-03-27 17:55:07 +0300

    oracle client

---
+++ OracleClientConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+Console.WriteLine("Hello, World!");
+await using var conn = new OracleConnection();
+await using var cmd = new OracleCommand("call", conn);
+await using var reader = await cmd.ExecuteReaderAsync();
+DataTable dt = new DataTable();
+dt.Load(reader);
