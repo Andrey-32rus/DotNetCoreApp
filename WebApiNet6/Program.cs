7df560f215eb70f29378d05ea54ffc6833daa612
SimutinAS <andrey.s32.work@yandex.ru>
2022-04-03 14:09:33 +0300

    web api net 6

---
+++ WebApiNet6/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseOpenApi(); // serve documents (same as app.UseSwagger())
+app.UseSwaggerUi3(); // serve Swagger UI
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
