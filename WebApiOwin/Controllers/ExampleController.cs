b2cc459fa100591cc198bf76d99a9b63798c4568
SimutinAS <andrey.s32.work@yandex.ru>
2022-04-03 14:43:36 +0300

    owin api

---
+++ WebApiOwin/Controllers/ExampleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace WebApiOwin.Controllers
+{
+    public class ExampleController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebApiOwin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace WebApiOwin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var app = WebApp.Start<Startup>(@"http://+:5000"))
+            {
+                Console.WriteLine("WebHost is started");
+                Console.ReadLine();
+            }
+        }
+    }
+}
